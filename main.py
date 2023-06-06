507d8f5f6a6a3169f6a9c5a589650efc15014613
MissiegoBeats <missiego.andres@gmail.com>
2023-06-06 10:47:48 +0200

    Funciona devolver valores

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def otra_funcion():
     # Lógica de la otra función
     print('Otra función ' + str(sys.argv))
 
+def cumpleagnos():
+    sys.argv[3] = int(str(sys.argv[3])) + 1
+    print(str(sys.argv)) #Nota: con print "devolvemos" el valor de la función que queremos usar en java!
+
 def main():
     if len(sys.argv) < 2:
         print("Debe proporcionar el nombre de la función a llamar como argumento")
@@ -15,10 +19,14 @@ def main():
 
     function_name = sys.argv[1]
 
+    #TODO: mejorar?
+    
     if function_name == "saludar":
         saludar()
     elif function_name == "otra_funcion":
         otra_funcion()
+    elif function_name == "cumpleagnos":
+        cumpleagnos()
     else:
         print("Función desconocida: " + function_name)
 
