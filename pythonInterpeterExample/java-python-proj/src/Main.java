9bc6f68180b67092545571d17809dfa4cec3bfa0
MissiegoBeats <missiego.andres@gmail.com>
2023-06-08 11:04:50 +0200

    nuevo planteamiento

---
+++ pythonInterpeterExample/java-python-proj/src/Main.java
@@ -0,0 +1,6 @@
+import org.python.util.PythonInterpreter;
+public class Main {
+    public static void main(String[] args) {
+        PythonInterpreter pythonInterpreter = new PythonInterpreter();
+    }
+}
