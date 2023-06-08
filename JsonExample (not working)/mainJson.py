2d0d5b95bb4d593956c83397c2ea27366d1e9528
MissiegoBeats <missiego.andres@gmail.com>
2023-06-08 09:33:27 +0200

    Cambio nombres y descubro PythonInterpreter

--- JsonExample (not working)/mainJson.py
+++ JsonExample (not working)/mainJson.py
@@ -6,8 +6,16 @@ def cambiarTelefono():
     jsonFile = open(sys.argv[2], "r") #Abrimos el archivo json (sys.argv[2] es el nombre del archivo
     datos = jsonFile.read() #Leemos el archivo
     datos_diccionario = json.loads(datos) #Cargamos el json como diccionario
+
     datos_diccionario["cliente"]["telefono"] = str(sys.argv[3]) #Modificamos el teléfono
     modificado = json.dumps(datos_diccionario) #Convertimos el diccionario a json
+
+    outputFile = open("outputData.json", "w") #Abrimos el archivo de salida
+    outputFile.write(modificado) #Escribimos el json modificado
+
+    outputFile.close() #Cerramos el archivo de salida
+    jsonFile.close() #Cerramos el archivo json
+
     print(modificado) #Imprimimos el json modificado
 
 
@@ -21,7 +29,6 @@ def main():
     if function_name == "cambiarTelefono":
         cambiarTelefono()
     else:
-        #print("Hey, voy a llamar a la función: " + function_name)
         print("Función desconocida: " + function_name + ".")
         
 if __name__ == '__main__':
