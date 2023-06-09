5f80416b1bce8788faa3a8127d12847c77365220
MissiegoBeats <missiego.andres@gmail.com>
2023-06-09 10:09:22 +0200

    Terminado

--- pythonInterpeterJava/pythonjavaintegration/src/pythonjavaintegration/PythonJavaIntegrationJsonExample.java
+++ pythonInterpeterJava/pythonjavaintegration/src/pythonjavaintegration/PythonJavaIntegrationJsonExample.java
@@ -1,5 +1,8 @@
 package pythonjavaintegration;
 
+import org.python.util.PythonInterpreter;
+import org.python.core.*;
+
 /**
  * Example with json
  * @author Andrés Missiego
@@ -10,7 +13,13 @@ public class PythonJavaIntegrationJsonExample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-    }
-    
+        String ruta = "./src/pythonjavaintegration/";
+        PythonInterpreter interpreter = new PythonInterpreter();
+        interpreter.execfile(ruta + "jsonprocessing.py");
+        PyString ficheroInput = new PyString("str('"+ruta+"inputData.json')");
+        PyString nuevoTelefono = new PyString("str('123-123-123')");
+        PyObject jsonMod = interpreter.eval("cambiarTelefono("+ficheroInput+","+nuevoTelefono+")");
+        Object jsonModJava = jsonMod.__tojava__(Object.class);
+        System.out.println(jsonModJava);        
+    }   
 }
---
+++ pythonInterpeterJava/pythonjavaintegration/src/pythonjavaintegration/jsonprocessing.py
@@ -0,0 +1,40 @@
+import sys
+import json
+
+def formatear_json(cadena_json):
+    try:
+        json_objeto = json.loads(cadena_json)
+        json_formateado = json.dumps(json_objeto, indent=4)
+        return json_formateado
+    except json.JSONDecodeError:
+        print("Error: La cadena no es un JSON valido.")
+        return None
+
+
+def cambiarTelefono(archivoInput, telefono):
+    jsonFile = open(archivoInput, "r") #Abrimos el archivo json
+    datos = jsonFile.read() #Leemos el archivo
+    datos_diccionario = json.loads(datos) #Cargamos el json como diccionario
+
+    datos_diccionario["cliente"]["telefono"] = str(telefono) #Modificamos el telefono
+    modificado = json.dumps(datos_diccionario) #Convertimos el diccionario a json
+
+    modificado = formatear_json(modificado) #Formateamos el json
+
+    print("Guardando el archivo modificado en outputData.json")
+    outputFile = open("./src/pythonjavaintegration/outputData.json", "w") #Abrimos el archivo de salida
+    outputFile.write(modificado) #Escribimos el json modificado
+    print("Archivo guardado")
+    outputFile.close() #Cerramos el archivo de salida
+
+    jsonFile.close() #Cerramos el archivo json
+
+    return modificado #Devolvemos el json modificado
+
+
+def main():
+    print("hola mundo, esto es el main de jsonprocessing.py")
+        
+if __name__ == '__main__':
+    #No se ejecuta si es un modulo importado
+    main()
