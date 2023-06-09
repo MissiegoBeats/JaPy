27f9dbbd51ef288c69363a591a93606dac11cb23
MissiegoBeats <missiego.andres@gmail.com>
2023-06-09 09:35:10 +0200

    comienzo el ejemplo con json

---
+++ pythonInterpeterJava/pythonjavaintegration/src/pythonjavaintegration/PythonJavaIntegrationJsonExample.java
@@ -0,0 +1,16 @@
+package pythonjavaintegration;
+
+/**
+ * Example with json
+ * @author Andrés Missiego
+ */
+public class PythonJavaIntegrationJsonExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
