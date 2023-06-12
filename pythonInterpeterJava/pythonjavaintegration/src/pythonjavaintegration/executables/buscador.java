9e5070d864771836321dc24fa59dcf805659f01c
MissiegoBeats <missiego.andres@gmail.com>
2023-06-12 15:56:07 +0200

    Cambios

--- pythonInterpeterJava/pythonjavaintegration/src/pythonjavaintegration/executables/buscador.java
+++ pythonInterpeterJava/pythonjavaintegration/src/pythonjavaintegration/executables/buscador.java
@@ -12,13 +12,13 @@ public class buscador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String idjava = "(JAVA)    ";
+        String idjava = "(JAVA)    "; //Para identificar si es de java o python
         BuscadorJsonEmpresas b = new BuscadorJsonEmpresas();
         System.out.println(idjava+"El correo obtenido es: "+b.obtenerCorreo("ABC Corporation", "Juan"));
         System.out.println(idjava+"El telefono obtenido es: "+b.obtenerTelefono("ABC Corporation"));
         System.out.println(idjava+"La direccion obtenida es: "+b.obtenerDireccion("Global Solutions"));
         System.out.println(idjava+"Modificando direccion de Global Solutions a Madrid");
         b.modificarDireccion("Global Solutions","Madrid");
-        System.out.println(idjava+"Direccion modificada, consulta el json");
+        System.out.println(idjava+"Direccion modificada, consulta el json"); //src/pythonjavaintegration/json/empresas.json
     }
 }
