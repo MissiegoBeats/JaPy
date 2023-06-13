508addc4d789fc6266238b0b30c6693009e7767b
MissiegoBeats <missiego.andres@gmail.com>
2023-06-13 11:53:15 +0200

    sys funciona

--- pythonInterpeterJava/pythonjavaintegration/src/pythonjavaintegration/pythoncodes/useoflibs.py
+++ pythonInterpeterJava/pythonjavaintegration/src/pythonjavaintegration/pythoncodes/useoflibs.py
@@ -1,6 +1,18 @@
-import pandas as pd
+import sys
 
-def useofpandas():
+def useofsys():
+    print("use of sys")
+    print("Path: "+str(sys.path))
+    print("Version: "+str(sys.version))
+    print("Version info: "+str(sys.version_info))
+    print("Executable: "+str(sys.executable))
+    print("Platform: "+str(sys.platform))
+
+useofsys()
+
+#import pandas as pd
+
+"""def useofpandas():
     print("use of pandas")
     df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
-    print(df)
+    print(df)"""
