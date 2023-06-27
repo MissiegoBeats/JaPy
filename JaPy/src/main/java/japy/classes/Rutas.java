bfdce60eadd0785bd8bd15e09e14b672f14cbf3e
MissiegoBeats <missiego.andres@gmail.com>
2023-06-27 14:02:49 +0200

    Example of argParse implementation in JaPy added

--- JaPy/src/main/java/japy/classes/Rutas.java
+++ JaPy/src/main/java/japy/classes/Rutas.java
@@ -10,6 +10,7 @@ public class Rutas {
     public final String useoflibs = "./src/main/java/japy/pythoncodes/useoflibs.py";
     public final String jsonAsParameterExample = "./src/main/java/japy/pythoncodes/jsonAsParameterExample.py";
     public final String jsonFileProcessing = "./src/main/java/japy/pythoncodes/jsonFileProcessing.py";
+    public final String findStadium = "./src/main/java/japy/pythoncodes/argParseExample.py";
     //Json paths
     public final String inputData = "./src/main/java/japy/json/inputData.json";
     public final String outputData = "./src/main/java/japy/json/outputData.json";
